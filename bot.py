8e429f69e0e7f6cda331a76cd0bf7d8cbd044690
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-08-15 14:20:45 +0430

    .

--- bot.py
+++ bot.py
@@ -50,5 +50,6 @@ bot.time = datetime.datetime.utcnow()
 for file in os.listdir("./config/core/"):
     if file.endswith(".py"):
         bot.load_extension(F"config.core.{file[:-3]}")
+sorted(bot.cogs)
 
 bot.run(os.getenv("TOKEN"))
