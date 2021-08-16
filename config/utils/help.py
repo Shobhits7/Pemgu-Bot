6a1e57a3e461a9056b18a695761ac18ec3438919
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-08-16 10:02:22 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -75,9 +75,8 @@ class MyHelp(commands.HelpCommand):
         )
         hcogmbed.set_thumbnail(url=ctx.me.avatar_url)
         hcogmbed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-        for commands in sorted(cog.get_commands()):
-            for command in cog.get_commands():
-                hcogmbed.add_field(name=self.get_command_signature(command), value=command.help or "No help found...")
+        for commands in cog.get_commands():
+                hcogmbed.add_field(name=self.get_command_signature(commands), value=commands.help or "No help found...")
         await ctx.reply(embed=hcogmbed)
 
     # Help Group
@@ -90,7 +89,7 @@ class MyHelp(commands.HelpCommand):
         )
         hgroupmbed.set_thumbnail(url=ctx.me.avatar_url)
         hgroupmbed.set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
-        for commands in sorted(group.commands):
+        for commands in group.commands:
             hgroupmbed.add_field(name=self.get_command_signature(commands), value=commands.help or "No help found...")
         await ctx.reply(embed=hgroupmbed)
 
