7b538824b7d694351fcf3cfbc4a8e980b82bacf1
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-08-16 10:46:45 +0430

    .

--- config/core/jishaku.py
+++ config/core/jishaku.py
@@ -4,6 +4,6 @@ from jishaku.cog import STANDARD_FEATURES
 
 class CustomDebugCog(*STANDARD_FEATURES, name="Jishaku", description="Jishaku commands are hidden in here"):
     pass
-    
+
 def setup(bot: commands.Bot):
     bot.add_cog(CustomDebugCog(bot=bot))
