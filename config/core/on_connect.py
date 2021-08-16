da75e86f3342c29ecfc49d33f9128ae350fdc38d
lvlahraam <lvlahraam@gmail.com>
2021-08-16 18:11:51 +0430

    .

--- config/core/on_connect.py
+++ config/core/on_connect.py
@@ -6,7 +6,7 @@ class OnReady(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_connect(self, ctx):
+    async def on_connect(self, ctx:commands.Context):
         print(F"---------------------------------------------------\nLogged in as: {self.bot.user} - {self.bot.user.id}\nMain prefix is: ~b\nThe Bot  is online now\n---------------------------------------------------")
 
         msgchannel = self.bot.get_channel(873472317114679336)
@@ -15,7 +15,7 @@ class OnReady(commands.Cog):
             title="Bot is online now",
             timestamp=ctx.message.created_at
         )
-        onlinembed.set_footer(text="You can use the commands now", icon_url=ctx.me.avatar_url)
+        onlinembed.set_footer(text="You can use the commands now", icon_url=self.bot.user.avatar_url)
         await msgchannel.send(embed=onlinembed)
 
 
