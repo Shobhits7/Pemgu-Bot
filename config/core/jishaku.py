3e01c4ffbe024b28f9c4a1d221a38c6c069fb38a
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-08-16 09:39:00 +0430

    .

--- config/core/jishaku.py
+++ config/core/jishaku.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from jishaku.cog import STANDARD_FEATURES
 
-class CustomDebugCog(*STANDARD_FEATURES, name="Jishaku 👀", description="Jishaku commands are hidden in here"):
+class CustomDebugCog(*STANDARD_FEATURES, description="Jishaku commands are hidden in here"):
     pass
 
 def setup(bot: commands.Bot):
