6b05091ec22c3b71fd464063fdb53a1cc9669c94
lvlahraam <lvlahraam@gmail.com>
2021-08-21 02:34:28 +0430

    .

--- config/core/setup.py
+++ config/core/setup.py
@@ -49,7 +49,7 @@ class Setup(commands.Cog, description="For setting up the bot"):
         await self.bot.db.execute("DELETE prefix FROM prefixes WHERE guild_id = $1", ctx.guild.id)
         pfrmbed = discord.Embed(
             colour=0x2F3136,
-            title="The prefix has been reseted to `~b`",
+            title="The prefix has been resetted  to `~b`",
             timestamp=ctx.message.created_at
         )
         pfrmbed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url)
