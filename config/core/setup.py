90f7da9898a60b523476d4ef3574b7f9d2deee05
lvlahraam <lvlahraam@gmail.com>
2021-08-21 12:43:32 +0430

    .

--- config/core/setup.py
+++ config/core/setup.py
@@ -46,7 +46,7 @@ class Setup(commands.Cog, description="For setting up the bot"):
     @commands.cooldown(1, 5, commands.BucketType.guild)
     async def reset(self, ctx):
         await ctx.trigger_typing()
-        await self.bot.db.execute("DELETE prefix FROM prefixes WHERE guild_id = $1", ctx.guild.id)
+        await self.bot.db.execute("FROM prefixes DELETE prefix WHERE guild_id = $1", ctx.guild.id)
         pfrmbed = discord.Embed(
             colour=0x2F3136,
             title="The prefix has been resetted  to `~b`",
