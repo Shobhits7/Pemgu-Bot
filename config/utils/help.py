73e828f0c62e7921e87e443241b1a548422ee39f
lvlahraam <lvlahraam@gmail.com>
2021-09-02 03:47:05 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -16,7 +16,6 @@ class MyHelp(commands.HelpCommand):
         super().__init__(
             command_attrs={
                 "help": "The help command for the bot",
-                "cooldown": commands.Cooldown(1, 3.0, commands.BucketType.user),
                 "aliases": ["h", "commands"]
             }
         )
