2cbb5adea76bc3463946371f71f343a6c2b0a8df
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-03 14:52:29 +0430

    .

--- config/core/utility.py
+++ config/core/utility.py
@@ -6,8 +6,6 @@ import time
 class Utility(commands.Cog, description="Useful commands are open to everyone"):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.help_command = MyHelp()
-        self.bot.help_command.cog = self
 
     # About
     @commands.command(name="about", aliases=["ab"], help="Will show the bot's information")
