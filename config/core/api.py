1c11b5f36ea21f9b9a0ed313cb77a5f9670fc978
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-03 14:37:38 +0430

    .

--- config/core/api.py
+++ config/core/api.py
@@ -74,7 +74,7 @@ class API(commands.Cog, description="Some cool API commands"):
     # Tweet
     @commands.command(name="tweet", aliases=["tw"], help="Will preview your tweet", usage="<username> <text>")
     @commands.bot_has_guild_permissions(attach_files=True)
-    async def tweet(self, ctx, user:commands.UserConverter = None, *, text,):
+    async def tweet(self, ctx, *, text, user:commands.UserConverter = None):
         await ctx.trigger_typing()
         user = user or ctx.author
         session = await session_bytes(F"https://api.dagpi.xyz/image/tweet/?url={user.avatar.with_static_format('png').with_size(1024)}&username={ctx.author.name}&text={text}", self.dagpi_headers)
