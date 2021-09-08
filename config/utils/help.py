844cafa92540eb3b800fdca62a039986071c9675
lvlahraam <lvlahraam@gmail.com>
2021-09-08 16:39:56 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -101,7 +101,7 @@ class MyHelp(commands.HelpCommand):
                 usable += len(filtered_commands)
         homepage.add_field(name="Prefix here:", value=ctx.clean_prefix or "(in DM you don't need to use prefix)")
         homepage.add_field(name="Usable:", value=usable)
-        homepage.add_field(name="Arguments:", value="[] means the argument is optional.\n<> means the argument is required.\n***DO NOT USE THIS WHEN USING A COMMAND***")
+        homepage.add_field(name="Arguments:", value="[] means the argument is optional.\n<> means the argument is required.\n***DO NOT USE THIS WHEN USING A COMMAND***", inline=False)
         view = HelpView(self, mapping, homepage, self.emojis)
         await ctx.send(embed=homepage, view=view)
         return
