f38bf21be7ebf839599e03838fbd091fc523f361
lvlahraam <lvlahraam@gmail.com>
2021-09-10 01:46:48 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -139,13 +139,14 @@ class MyHelp(commands.HelpCommand):
         ctx = self.context
         hcmdmbed = discord.Embed(
             colour=0xF49B33,
+            title=F"`{self.get_command_signature(command)}`",
+            description=command.help or "No help found...",
             timestamp=ctx.message.created_at
         )
         hcmdmbed.set_thumbnail(url=ctx.me.avatar.url)
         hcmdmbed.set_author(name=ctx.author, icon_url=ctx.author.avatar.url)
         if cog := command.cog:
-            hcmdmbed.title = F"{self.emojis.get(cog.qualified_name) if self.emojis.get(cog.qualified_name) else ''} {cog.qualified_name} Category"
-            hcmdmbed.description = F"{cog.description or 'No description found...'}\n\n"
+            hcmdmbed.add_field(name="Category:", value=F"{self.emojis.get(cog.qualified_name) if self.emojis.get(cog.qualified_name) else '❓'} {cog.qualified_name}")
         can_run = "No"
         with contextlib.suppress(commands.CommandError):
             if await command.can_run(self.context):
@@ -153,7 +154,6 @@ class MyHelp(commands.HelpCommand):
         hcmdmbed.add_field(name="Usable", value=can_run)
         if command._buckets and (cooldown := command._buckets._cooldown):
             hcmdmbed.add_field(name="Cooldown", value=F"{cooldown.rate} per {cooldown.per:.0f} seconds")
-        hcmdmbed.description += F"`{self.get_command_signature(command)}` - {command.help or 'No help found...'}"
         await ctx.send(embed=hcmdmbed)
         return
 
@@ -163,20 +163,20 @@ class MyHelp(commands.HelpCommand):
         can_run = "No"
         hgroupmbed = discord.Embed(
             colour=0xF49B33,
+            title=F"`{self.get_command_signature(group)}`",
+            description=F"{group.help or 'No help found...'}\n\n",
             timestamp=ctx.message.created_at
         )
         hgroupmbed.set_thumbnail(url=ctx.me.avatar.url)
         hgroupmbed.set_author(name=ctx.author, icon_url=ctx.author.avatar.url)
         for command in group.commands:
-            hgroupmbed.description = F"`{self.get_command_signature(command)}` - {command.help or 'No help found...'}\n"
+            hgroupmbed.description += F"`{self.get_command_signature(command)}` - {command.help or 'No help found...'}\n"
         if cog := command.cog:
-            hgroupmbed.title = F"{self.emojis.get(cog.qualified_name) if self.emojis.get(cog.qualified_name) else '❓'} {cog.qualified_name} Category"
-            hgroupmbed.description += F"{cog.description}"
-        hgroupmbed.description += F"`{self.get_command_signature(group)}` - {group.help or 'No help found...'}\n",
-        with contextlib.suppress(commands.CommandError):
-            if await command.can_run(self.context):
-                can_run = "Yes"
-        hgroupmbed.add_field(name="Usable", value=can_run)
+            hgroupmbed.add_field(name="Category", value=F"{self.emojis.get(cog.qualified_name) if self.emojis.get(cog.qualified_name) else '❓'} {cog.qualified_name}")
+            with contextlib.suppress(commands.CommandError):
+                if await command.can_run(self.context):
+                    can_run = "Yes"
+            hgroupmbed.add_field(name="Usable", value=can_run)
         if command._buckets and (cooldown := command._buckets._cooldown):
             hgroupmbed.add_field(name="Cooldown", value=F"{cooldown.rate} per {cooldown.per:.0f} seconds")
         await ctx.send(embed=hgroupmbed)
