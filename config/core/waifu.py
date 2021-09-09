60e606640bd1cebd6408d9349ac4d1dc8ad913b7
lvlahraam <lvlahraam@gmail.com>
2021-09-10 03:38:24 +0430

    .

--- config/core/waifu.py
+++ config/core/waifu.py
@@ -6,6 +6,7 @@ class Anime(commands.Cog, description="SFW Waifu's and Husbando's chamber"):
     def __init__(self, bot):
         self.bot = bot
     
+    # SFW
     @commands.group(name="sfw", help="Will send an random waifu or husbando image if not specified", invoke_without_command=True)
     async def sfw(self, ctx):
         await ctx.trigger_typing()
@@ -18,6 +19,32 @@ class Anime(commands.Cog, description="SFW Waifu's and Husbando's chamber"):
         sfwmbed.set_image(url=session["url"])
         sfwmbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
         await ctx.send(embed=sfwmbed)
+    # SFW Waifu
+    @sfw.command(name="waifu", aliases=["wa"], help="Will send an random waifu image")
+    async def waifu_sfw(self, ctx):
+        await ctx.trgger_typing()
+        session = await session_json("https://api.hori.ovh/sfw/waifu/")
+        sfwwambed = discord.Embed(
+            colour=self.bot.color,
+            title="Here is your SFW Waifu Image",
+            timestamp=ctx.message.created_at
+        )
+        sfwwambed.set_image(url=session["url"])
+        sfwwambed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
+        await ctx.send(embed=sfwwambed)
+    # SFW Husbando
+    @sfw.command(name="husbando", aliases=["ha"], help="Will send an random husbando image")
+    async def husbando_sfw(self, ctx):
+        await ctx.trgger_typing()
+        session = await session_json("https://api.hori.ovh/sfw/husbando/")
+        sfwhambed = discord.Embed(
+            colour=self.bot.color,
+            title="Here is your SFW Haifu Image",
+            timestamp=ctx.message.created_at
+        )
+        sfwhambed.set_image(url=session["url"])
+        sfwhambed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
+        await ctx.send(embed=sfwhambed)
 
 def setup(bot):
     bot.add_cog(Anime(bot))
