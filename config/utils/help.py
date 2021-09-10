498595ba717fc6439f8865fe13bbcc5714b1c46b
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-10 22:37:14 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -138,7 +138,7 @@ class MyHelp(commands.HelpCommand):
         ctx = self.context
         hcmdmbed = discord.Embed(
             colour=0xF49B33,
-            title=F"***{self.get_command_signature(command)}***",
+            title=F"**{self.get_command_signature(command)}**",
             description=command.help or "No help found...",
             timestamp=ctx.message.created_at
         )
@@ -169,7 +169,7 @@ class MyHelp(commands.HelpCommand):
         hgroupmbed.set_thumbnail(url=ctx.me.avatar.url)
         hgroupmbed.set_author(name=ctx.author, icon_url=ctx.author.avatar.url)
         for command in group.commands:
-            hgroupmbed.description += F"***{self.get_command_signature(command)}*** - {command.help or 'No help found...'}\n"
+            hgroupmbed.description += F"**{self.get_command_signature(command)}** - {command.help or 'No help found...'}\n"
         if cog := command.cog:
             hgroupmbed.add_field(name="Category", value=F"{self.emojis.get(cog.qualified_name) if self.emojis.get(cog.qualified_name) else '❓'} {cog.qualified_name}")
             with contextlib.suppress(commands.CommandError):
