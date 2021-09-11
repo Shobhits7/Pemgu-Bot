dd707f62a550b1657e5176684cdbedccc5541e51
lvlahraam <lvlahraam@gmail.com>
2021-09-11 14:20:30 +0430

    .

--- config/core/database.py
+++ config/core/database.py
@@ -5,17 +5,30 @@ class Database(commands.Cog, description="Monotoring database with these"):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command(name="delete", aliases=["del"], help="Will delete a row from db")
+    @commands.command(name="delete_prefix", help="Will delete a row from db")
     @commands.is_owner()
-    async def delete(self, ctx, *, table):
+    async def delete_prefix(self, ctx, *, guild):
         await ctx.trigger_typing()
         dltmbed = discord.Embed(
             colour=self.bot.color,
-            title=F"Deleted from {table}",
+            title=F"Deleted from prefixes",
             timestamp=ctx.message.created_at
         )
         dltmbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
-        await self.bot.db.execute(F"DELETE FROM {table} WHERE guild_id = $1", ctx.guild.id)
+        await self.bot.db.execute(F"DELETE FROM preffixes WHERE guild_id = $1", guild)
+        await ctx.send(embed=dltmbed)
+    
+    @commands.command(name="delete_todo", help="Will delete a row from db")
+    @commands.is_owner()
+    async def delete_todo(self, ctx, *, user):
+        await ctx.trigger_typing()
+        dltmbed = discord.Embed(
+            colour=self.bot.color,
+            title=F"Deleted from todo",
+            timestamp=ctx.message.created_at
+        )
+        dltmbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
+        await self.bot.db.execute(F"DELETE FROM todo WHERE user_id = $1", user)
         await ctx.send(embed=dltmbed)
 
 def setup(bot):
