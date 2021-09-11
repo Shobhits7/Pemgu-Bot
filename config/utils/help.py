114027719dfd47ced447094b133bf99dd3570468
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-11 21:09:02 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -51,7 +51,7 @@ class HelpView(discord.ui.View):
     async def on_timeout(self):
         for item in self.children:
             if isinstance(item, discord.SelectMenu):
-                item.placeholder = "This help command is disabled now..."
+                item.placeholder.edit("This help command is disabled now...")
             item.disabled = True
         await self.message.edit(view=self)
 
