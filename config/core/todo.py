f8d295c59b2c2a5630d8c25ff5c3e3c6d4d5966b
lvlahraam <lvlahraam@gmail.com>
2021-09-11 13:33:50 +0430

    .

--- config/core/todo.py
+++ config/core/todo.py
@@ -9,7 +9,21 @@ class Todo(commands.Cog, description="Lazy people use these"):
     @commands.group(name="todo", help="Will tell you, your todo list", invoke_without_command=True)
     async def todo(self, ctx):
         await ctx.trigger_typing()
-        await ctx.send("IN DEVELOPMENT")
+        todo = await self.bot.db.fetch("SELECT task, position FROM todo WHERE user_id = $1", ctx.author.id)
+        if len(todo) == 0:
+            await ctx.send("You don't have a todo list\nTry to make one with `todo add` command")
+        else:
+            embed = discord.Embed(
+                title="Here is your todo list",
+            )
+            embed.description = "\n".join("{position}: {task}".format(**_) for _ in todo)
+    
+    # Add
+    @todo.command(name="add", help="Will add the given task", usage="<task>")
+    async def add(self, ctx, *, task):
+        await ctx.trigger_typing()
+        await self.bot.db.execute("INSERT INTO todo(user_id, task) VALUES ($1, $2)", ctx.author.id, task)
+        await ctx.send("Your task has been now added")
 
 def setup(bot):
     bot.add_cog(Todo(bot))
