bb34f0cdd250df86d818f4d406b3e13ec6174d81
lvlahraam <lvlahraam@gmail.com>
2021-09-11 21:43:14 +0430

    .

--- config/utils/help.py
+++ config/utils/help.py
@@ -49,11 +49,14 @@ class HelpView(discord.ui.View):
         self.add_item(discord.ui.Button(label="Support Server", style=discord.ButtonStyle.green, url="https://discord.gg/bWnjkjyFRz"))
 
     async def on_timeout(self):
-        for item in self.children:
-            if isinstance(item, discord.SelectMenu):
-                item.placeholder = "This help command is disabled now..."
-            item.disabled = True
-        await self.message.edit(view=self)
+        if self.message:
+            for item in self.children:
+                if isinstance(item, discord.SelectMenu):
+                    item.placeholder = "This help command is disabled now..."
+                item.disabled = True
+            await self.message.edit(view=self)
+        else:
+            return
 
     async def interaction_check(self, interaction: discord.Interaction):
         if interaction.user.id == self.help.context.author.id:
