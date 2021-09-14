1e63b3f1824399b0b972a05349d6bf0e17684e1e
lvlahraam <lvlahraam@gmail.com>
2021-09-14 15:46:05 +0430

    ADDED FUN - COUNTER

--- config/views/counter.py
+++ config/views/counter.py
@@ -5,16 +5,19 @@ class CounterView(discord.ui.View):
     def __init__(self, client):
         super().__init__(timeout=5)
         self.clicks = 0
+        self.clickers = ""
         self.client = client
 
     @discord.ui.button(label="➕1", style=discord.ButtonStyle.blurple)
     async def Plus1(self, button: discord.ui.Button, interaction: discord.Interaction):
         print("Plus 1 was pressed")
+        self.clickers += interaction.user
         self.clicks += 1
     
     @discord.ui.button(label="➖1", style=discord.ButtonStyle.red)
     async def Minus1(self, button: discord.ui.Button, interaction: discord.Interaction):
         print("Minus 1 was pressed")
+        self.clickers += interaction.user
         self.clicks += 1
 
     async def on_timeout(self):
@@ -22,6 +25,7 @@ class CounterView(discord.ui.View):
             item.disabled = True
         ontimeoutmbed = discord.Embed(
             colour=self.client.color,
-            title=F"Button was clicked {self.clicks} times"
+            title=F"Button was clicked {self.clicks} times",
+            description=", \n".join(clicker for clicker in self.clickers)
         )
         await self.message.edit(embed=ontimeoutmbed, view=self)
