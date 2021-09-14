529a21d632f5b354edbfb6995ab5f6de02331fd3
lvlahraam <lvlahraam@gmail.com>
2021-09-14 16:26:33 +0430

    ADDED Fun - Counter

--- config/views/counter.py
+++ config/views/counter.py
@@ -6,6 +6,8 @@ class CounterView(discord.ui.View):
         super().__init__(timeout=5)
         self.clicks = 0
         self.clickers = ""
+        self.plus = 0
+        self.minus = 0
         self.client = client
 
     @discord.ui.button(label="➕1", style=discord.ButtonStyle.blurple)
@@ -13,12 +15,16 @@ class CounterView(discord.ui.View):
         print("Plus 1 was pressed")
         self.clickers += F"**{interaction.user.name}** Plused\n"
         self.clicks += 1
+        self.plus += 1
+        button.label = F"➕{self.plus}"
     
     @discord.ui.button(label="➖1", style=discord.ButtonStyle.red)
     async def Minus1(self, button: discord.ui.Button, interaction: discord.Interaction):
         print("Minus 1 was pressed")
         self.clickers += F"**{interaction.user.name}** Minused\n"
-        self.clicks += 1
+        self.clicks -= 1
+        self.minus += 1
+        button.label = F"➖{self.minus}"
 
     async def on_timeout(self):
         for item in self.children:
