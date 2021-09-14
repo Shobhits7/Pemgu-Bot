0191f15422b5d06ca6a20325f4b76fe3e8182b76
lvlahraam <lvlahraam@gmail.com>
2021-09-15 02:54:30 +0430

    Added Help Command with Buttons

--- config/views/helpbuttons.py
+++ config/views/helpbuttons.py
@@ -25,7 +25,7 @@ class HelpButtons(discord.ui.Button):
                 callbackmbed.set_thumbnail(url=self.help.context.me.avatar.url)
                 callbackmbed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
                 await interaction.response.edit_message(embed=callbackmbed)
-        if self.custom_id == "Home":
+        if self.custom_id == "Home Page":
             await interaction.response.edit_message(embed=self.homepage)
         if self.custom_id == "Delete":
             deletembed = discord.Embed(
@@ -46,7 +46,7 @@ class HelpView(discord.ui.View):
         self.mapping = mapping
         self.homepage = homepage
         self.emojis = emojis
-        self.add_item(item=HelpButtons(emoji="🏠", label="Home", style=discord.ButtonStyle.green, custom_id="Home", view=self))
+        self.add_item(item=HelpButtons(emoji="🏠", label="Home Page", style=discord.ButtonStyle.green, custom_id="Home Page", view=self))
         for cog, commands in self.mapping.items():
             name = cog.qualified_name if cog else "No"
             description = cog.description if cog else "Commands without category"
