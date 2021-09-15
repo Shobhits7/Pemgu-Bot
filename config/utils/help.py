144a7b1f9f8d87d0784c67e8c3a9b56a69782953
lvlahraam <lvlahraam@gmail.com>
2021-09-15 05:15:23 +0430

    SPAWN

--- config/utils/help.py
+++ config/utils/help.py
@@ -1,6 +1,6 @@
-import discord, contextlib
+import discord, contextlib, random
 from discord.ext import commands
-from config.views import helpmenu
+from config.views import helpmenu, helpbuttons
 
 class MyHelp(commands.HelpCommand):
     def __init__(self):
@@ -42,7 +42,10 @@ class MyHelp(commands.HelpCommand):
         homepage.add_field(name="Prefix:", value=ctx.prefix or "In DM you don't need to use prefix", inline=False)
         homepage.add_field(name="Usable:", value=usable, inline=False)
         homepage.add_field(name="Arguments:", value="[] means the argument is optional.\n<> means the argument is required.\n***DO NOT USE THESE WHEN DOING A COMMAND***", inline=False)
-        view = helpmenu.HelpView(self, mapping, homepage, self.emojis)
+        choices = [helpmenu.HelpView, helpbuttons.HelpView]
+        choice = random.choice(choices)
+        # helpmenu.HelpView(self, mapping, homepage, self.emojis)
+        view = choice(self, mapping, homepage, self.emojis)
         view.message = await ctx.send(embed=homepage, view=view)
         return
 
