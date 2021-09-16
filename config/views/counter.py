19c8932c7d803de345dd6257bcb0a4c220809404
lvlahraam <lvlahraam@gmail.com>
2021-09-16 16:11:08 +0430

    Changed counter

--- config/views/counter.py
+++ config/views/counter.py
@@ -6,34 +6,24 @@ class CounterView(discord.ui.View):
         super().__init__(timeout=5)
         self.clicks = 0
         self.clickers = ""
-        self.plus = 0
-        self.minus = 0
         self.client = client
 
-    @discord.ui.button(emoji="➕", style=discord.ButtonStyle.green)
-    async def Plus1(self, button: discord.ui.Button, interaction: discord.Interaction):
-        self.clickers += F"**{interaction.user.name}** Plused\n"
+    @discord.ui.Button(emoji="🍩", style=discord.ButtonStyle.blurple)
+    async def click(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.clicks += 1
-        self.plus += 1
-        button.label = F"{self.plus}"
-    
-    @discord.ui.button(emoji="➖", style=discord.ButtonStyle.red)
-    async def Minus1(self, button: discord.ui.Button, interaction: discord.Interaction):
-        self.clickers += F"**{interaction.user.name}** Minused\n"
-        self.clicks -= 1
-        self.minus += 1
-        button.label = F"{self.minus}"
+        self.clickers += F"**{interaction.user.name}** Clicked\n"
+        button.label = self.clicks
 
     async def on_timeout(self):
         for item in self.children:
             item.disabled = True
         ontimeoutmbed = discord.Embed(
             colour=self.client.color,
-            title=F"Score: **{self.clicks}** between Pluses vs Minuses",
+            title=F"Button was clicked: {self.clicks} times",
         )
         if len(self.clickers) != 0 or self.clicks != 0:
             ontimeoutmbed.description = "People who clicked:\n"
             for clicker in self.clickers:
-                ontimeoutmbed.description += F"{clicker}"
+                ontimeoutmbed.description += clicker
         else: ontimeoutmbed.description = "Nobody clicked the buttons"
         await self.message.edit(embed=ontimeoutmbed, view=self)
