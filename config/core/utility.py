a353aa7273ca35fe5a2179577576baeaececdce7
lvlahraam <lvlahraam@gmail.com>
2021-09-16 16:28:35 +0430

    Changed Help Command place

--- config/core/utility.py
+++ config/core/utility.py
@@ -1,9 +1,12 @@
 import discord, time
 from discord.ext import commands
+from config.utils.help import MyHelp
 
 class Utility(commands.Cog, description="Useful commands that are open to everyone"):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.help_command = MyHelp()
+        self.bot.help_command.cog = self
 
     # About
     @commands.command(name="about", aliases=["ab"], help="Will show the bot's information")
