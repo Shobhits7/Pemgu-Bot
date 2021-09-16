222d3cdbfc05c2575c84c658847a185823bb036c
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-16 22:41:56 +0430

    Update helpmenu.py

--- config/views/helpmenu.py
+++ config/views/helpmenu.py
@@ -31,7 +31,7 @@ class HelpMenu(discord.ui.Select):
                     timestamp=self.help.context.message.created_at
                 )
                 for command in cog.walk_commands():
-                    mbed.description += F"• **{self.help.get_command_signature(command)}** - {command.help or 'No help found...'}\n"
+                    mbed.description += F"**{self.help.get_command_signature(command)}** - {command.help or 'No help found...'}\n"
                 mbed.set_thumbnail(url=self.help.context.me.avatar.url)
                 mbed.set_author(name=interaction.user, icon_url=interaction.user.avatar.url)
                 await interaction.response.edit_message(embed=mbed)
