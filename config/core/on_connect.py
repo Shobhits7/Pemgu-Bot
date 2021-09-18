05f27351fcb3b090d5702e6384fc18247fce760e
lvlahraam <lvlahraam@gmail.com>
2021-09-18 07:52:05 +0430

    Update on_connect.py

--- config/core/on_connect.py
+++ config/core/on_connect.py
@@ -9,9 +9,5 @@ class OnConnect(commands.Cog):
     async def on_connect(self):
         print(F"---------------------------------------------------\nLogged in as: {self.bot.user} - {self.bot.user.id}\nMain prefix is: {self.bot.prefix}\nGuilds bot is in: {len(self.bot.guilds)}\nThe Bot is online now\n---------------------------------------------------")
 
-    @commands.Cog.listener()
-    async def on_disconnect(self):
-        await self.bot.aiosession.close()
-
 def setup(bot):
     bot.add_cog(OnConnect(bot))
