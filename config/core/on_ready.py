79351f9cbf4b4dacaba0b456422062168731eab8
lvlahraam <lvlahraam@gmail.com>
2021-09-18 07:54:04 +0430

    .

--- config/core/on_ready.py
+++ config/core/on_ready.py
@@ -1,13 +1,13 @@
 import discord
 from discord.ext import commands
 
-class OnConnect(commands.Cog):
+class OnReady(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_connect(self):
+    async def on_ready(self):
         print(F"---------------------------------------------------\nLogged in as: {self.bot.user} - {self.bot.user.id}\nMain prefix is: {self.bot.prefix}\nGuilds bot is in: {len(self.bot.guilds)}\nThe Bot is online now\n---------------------------------------------------")
 
 def setup(bot):
-    bot.add_cog(OnConnect(bot))
+    bot.add_cog(OnReady(bot))
