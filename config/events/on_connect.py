f4bef78d6b0e22f73ce6f5a6ad2b8372f9bbae3a
lvlahraam <lvlahraam@gmail.com>
2021-09-18 08:08:19 +0430

    .

--- config/events/on_connect.py
+++ config/events/on_connect.py
@@ -1,13 +1,17 @@
 import discord
 from discord.ext import commands
 
-class OnReady(commands.Cog):
+class OnConnect(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_ready(self):
+    async def on_connect(self):
         print(F"---------------------------------------------------\nLogged in as: {self.bot.user} - {self.bot.user.id}\nMain prefix is: {self.bot.prefix}\nGuilds bot is in: {len(self.bot.guilds)}\nThe Bot is online now\n---------------------------------------------------")
+    
+    @commands.Cog.listener()
+    async def on_disconnect(self):
+        await self.bot.aiosession.close()
 
 def setup(bot):
-    bot.add_cog(OnReady(bot))
+    bot.add_cog(OnConnect(bot))
