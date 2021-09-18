89b8fc4194ca78408b764e7ee8e50bcc623e6c56
lvlahraam <lvlahraam@gmail.com>
2021-09-19 04:14:12 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,28 +16,31 @@ async def get_prefix_postgresql(bot, message):
         prefix = prefix[0].get("prefix")
     return commands.when_mentioned_or(prefix)(bot, message)
 
+async def httpsession():
+    bot.session = aiohttp.ClientSession()
+    print("Making a Session was successful")
+
+def loader(self):
+    for folder in sorted(os.listdir("./config/")):
+        if folder in ("commands", "events"):
+            for cog in sorted(os.listdir(F"./config/{folder}/")):
+                if cog.endswith(".py"):
+                    self.load_extension(F"config.{folder}.{cog[:-3]}")
+
 class BotBase(commands.Bot):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.prefix = ";w"
         self.color = 0x2F3136
         self.load_extension("jishaku")
+        loader(self=self)
         os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
         os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
-        for folder in sorted(os.listdir("./config/")):
-            if folder in ("commands", "events"):
-                for cog in sorted(os.listdir(F"./config/{folder}/")):
-                    if cog.endswith(".py"):
-                        self.load_extension(F"config.{folder}.{cog[:-3]}")
     async def close(self):
         if not self.session.closed:
             await self.session.close()
 
 bot = BotBase(slash_commands=True, slash_command_guilds=[804380398296498256], command_prefix=get_prefix_postgresql, strip_after_prefix=True, case_insensitive=True, help_command=MyHelp(), intents=discord.Intents.all(), allowed_mentions=discord.AllowedMentions(users=False, everyone=False, roles=False, replied_user=False))
-
-async def httpsession():
-    bot.session = aiohttp.ClientSession()
-    print("Making a Session was successful")
 
 bot.loop.run_until_complete(create_db_poll())
 bot.loop.run_until_complete(httpsession())
