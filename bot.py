761beaa4ec8975c144fee8b02417501171a8ccf0
lvlahraam <lvlahraam@gmail.com>
2021-09-19 10:11:21 +0430

    .

--- bot.py
+++ bot.py
@@ -32,11 +32,9 @@ async def httpsession():
     bot.session = aiohttp.ClientSession()
     print("Making a Session was successful")
 
-for folder in sorted(os.listdir("./config/")):
-    if folder in ("commands", "events"):
-        for cog in sorted(os.listdir(F"./config/{folder}/")):
-            if cog.endswith(".py"):
-                bot.load_extension(F"config.{folder}.{cog[:-3]}")
+for cog in sorted(os.listdir("./config/core/")):
+    if cog.endswith(".py"):
+        bot.load_extension(F"config.core.{cog[:-3]}")
 
 bot.load_extension("jishaku")
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
