2a4a4bcd247185dfec6dacabcab1887583938ad5
lvlahraam <lvlahraam@gmail.com>
2021-09-19 08:05:49 +0430

    Update viewfun.py

--- config/views/viewfun.py
+++ config/views/viewfun.py
@@ -7,7 +7,7 @@ class CounterView(discord.ui.View):
         self.clickers = ""
         self.client = client
 
-    @discord.ui.button(emoji="🍏", style=discord.ButtonStyle.green)
+    @discord.ui.button(emoji="🍘🍙", style=discord.ButtonStyle.green)
     async def click(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.clicks += 1
         if str(interaction.user) in self.clickers:
