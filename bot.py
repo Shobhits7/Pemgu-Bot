15000304a8095ee71e0ca2d2f1c256d95d6af6b2
lvlahraam <lvlahraam@gmail.com>
2021-09-19 09:56:48 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,16 +38,16 @@ for folder in sorted(os.listdir("./config/")):
             if cog.endswith(".py"):
                 bot.load_extension(F"config.{folder}.{cog[:-3]}")
 
-# bot.lava_nodes = [
-#     {
-#         "host": "lava.link",
-#         "port": 80,
-#         "rest_url": "https://lava.link:80",
-#         "identifier": "MAIN",
-#         "password": "anything",
-#         "region": "singapore"
-#     }
-# ]
+bot.lava_nodes = [
+    {
+        "host": "lava.link",
+        "port": 80,
+        "rest_url": "https://lava.link:80",
+        "identifier": "MAIN",
+        "password": "anything",
+        "region": "singapore"
+    }
+]
 bot.load_extension("dismusic")
 
 bot.load_extension("jishaku")
