830649712c3451e803f3f88a2b2b2800639940e5
lvlahraam <lvlahraam@gmail.com>
2021-09-20 02:32:50 +0430

    Update errors.py

--- config/utils/errors.py
+++ config/utils/errors.py
@@ -30,8 +30,7 @@ async def handler(bot, ctx, error):
         )
         nmatcnfmbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
         if len(matches) > 0:
-            view = dymview.DYMView(bot=bot, ctx=ctx, matches=matches)
-            view.message = await ctx.send(embed=matcnfmbed, view=view)
+            await ctx.send(embed=matcnfmbed)
         else:
             await ctx.send(embed=nmatcnfmbed)
     elif isinstance(error, commands.MissingPermissions):
