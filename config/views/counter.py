c53604137c7cdc9edf815af8c6e2d45807b5bb4a
lvlahraam <lvlahraam@gmail.com>
2021-09-19 06:33:04 +0430

    Update counter.py

--- config/views/counter.py
+++ config/views/counter.py
@@ -1,19 +1,16 @@
-import discord
-from discord.ext import commands
-from collections import Counter
+import discord, collections
 
 class CounterView(discord.ui.View):
     def __init__(self, client):
         super().__init__(timeout=5)
         self.clicks = 0
-        self.clickers = Counter()
+        self.clickers = collections.Counter()
         self.client = client
 
     @discord.ui.button(emoji="🍏", style=discord.ButtonStyle.green)
     async def click(self, button: discord.ui.Button, interaction: discord.Interaction):
         self.clicks += 1
         self.clickers[interaction.user.name] += 1
-        dict(self.clickers)
         button.label = self.clicks
 
     async def on_timeout(self):
@@ -25,7 +22,6 @@ class CounterView(discord.ui.View):
         )
         if len(self.clickers) != 0 or self.clicks != 0:
             ontimeoutmbed.description = "People who clicked:\n"
-            for clicker in self.clickers:
-                ontimeoutmbed.description += clicker
+            ontimeoutmbed.description += ", \n".join(dict(self.clickers))
         else: ontimeoutmbed.description = "Nobody clicked the buttons"
         await self.message.edit(embed=ontimeoutmbed, view=self)
