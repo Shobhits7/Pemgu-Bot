9907ca12a6428842507e899365bc29593ccca209
lvlahraam <lvlahraam@gmail.com>
2021-09-21 23:49:32 +0330

    .

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import discord, aiohttp, asyncpg, motor.motor_asyncio, os
 from discord.ext import commands
 from config.utils import errors, help, options
 
-async def create_postgresql_pool():
+async def create_postgresl_pool():
     bot.postgresql = await asyncpg.create_pool(dsn=os.getenv("DATABASE_URL"))
     print("Creating pool to Postgres was successful")
 
-async def get_prefix_postgresql(bot, message):
+async def get_prefix_postgres(bot, message):
     if not message.guild:
         return ""
     prefix = await bot.postgresql.fetch("SELECT prefix FROM prefixes WHERE guild_id = $1", message.guild.id)
@@ -14,21 +14,6 @@ async def get_prefix_postgresql(bot, message):
         prefix = bot.prefix
     else:
         prefix = prefix[0].get("prefix")
-    return commands.when_mentioned_or(prefix)(bot, message)
-
-async def connect_mongodb_cluster():
-    bot.cluster = motor.motor_asyncio.AsyncIOMotorClient(os.getenv("MONGODB"))
-    print("Connection to MongoDB was successful")
-
-async def get_prefix_mongodb(bot, message):
-    if not message.guild:
-        return ""
-
-    prefixes = bot.cluster["Mei-DB"]['prefixes']
-    if (await prefixes.count_documents({}) == 0):
-        prefix = bot.prefix
-    else:
-        prefix = await prefixes.find_one({'_id': str(message.guild.id)})
     return commands.when_mentioned_or(prefix)(bot, message)
 
 async def aiohttpsession():
@@ -68,7 +53,7 @@ class Bot(commands.AutoShardedBot):
         if not self.session.closed:
             await self.session.close()
 
-bot = Bot(slash_commands=True, slash_command_guilds=[804380398296498256], command_prefix=get_prefix_postgresql, strip_after_prefix=True, case_insensitive=True, help_command=help.CustomHelp(), intents=discord.Intents.all(), allowed_mentions=discord.AllowedMentions(users=False, everyone=False, roles=False, replied_user=False))
+bot = Bot(slash_commands=True, slash_command_guilds=[804380398296498256], command_prefix=get_prefix_postgres, strip_after_prefix=True, case_insensitive=True, help_command=help.CustomHelp(), intents=discord.Intents.all(), allowed_mentions=discord.AllowedMentions(users=False, everyone=False, roles=False, replied_user=False))
 
 bot.prefix = ".m"
 bot.colour = 0x2F3136
@@ -81,6 +66,6 @@ bot.load_extension("jishaku")
 os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
 os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True" 
 
-bot.loop.run_until_complete(create_postgresql_pool())
+bot.loop.run_until_complete(create_postgresl_pool())
 bot.loop.create_task(aiohttpsession())
 bot.run(os.getenv("TOKEN"))
