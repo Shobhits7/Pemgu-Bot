a8bf5641135487e8692639f323c41531d225e7db
lvlahraam <lvlahraam@gmail.com>
2021-09-22 08:29:50 +0330

    Update utility.py

--- config/core/utility.py
+++ config/core/utility.py
@@ -151,7 +151,7 @@ class Utility(commands.Cog, description="Useful commands that are open to everyo
                 # durationminutes = (durationseconds//60)%60
                 # finspotifymbed.add_field(name="Duration:", value=F"{durationhours}:{durationminutes}:{durationseconds}", inline=False)
                 durationtime = time.gmtime(activity.duration.total_seconds())
-                finspotifymbed.add_field(name="Duration:", value=time.strftime('%H:%M:%S', durationtime, inline=False)
+                finspotifymbed.add_field(name="Duration:", value=time.strftime('%H:%M:%S', durationtime), inline=False)
                 finspotifymbed.add_field(name="Created-at:", value=discord.utils.format_dt(activity.created_at, style="f"), inline=False)
                 finspotifymbed.add_field(name="Track-ID", value=activity.track_id, inline=False)
                 finspotifymbed.set_image(url=activity.album_cover_url)
