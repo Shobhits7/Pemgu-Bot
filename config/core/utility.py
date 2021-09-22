e9214bd0d4efa0e6f75b17bdb6686dd42ac7a47a
lvlahraam <lvlahraam@gmail.com>
2021-09-23 00:39:46 +0330

    Update utility.py

--- config/core/utility.py
+++ config/core/utility.py
@@ -70,14 +70,14 @@ class Utility(commands.Cog, description="Useful commands that are open to everyo
         ***Web-Status:*** {member.web_status}
         ***Desktop-Status:*** {member.desktop_status}
         ***Mobile-Status:*** {member.mobile_status}
-        ***Registered:*** {discord.utils.format_dt(member.created_at, style="f")}""")
+        ***Registered:*** {discord.utils.format_dt(member.created_at, style="f")}""", inline=False)
         iombed.add_field(name="Guild-Information:", value=F"""
         ***Joined:*** {discord.utils.format_dt(member.joined_at, style="f")}
         ***Roles [{len(member.roles)}]:*** {', '.join(role.mention for role in member.roles)}
         ***Top-Role:*** {member.top_role.mention}
         ***Boosting:*** {'True' if member in ctx.guild.premium_subscribers else 'False'}
         ***Voice:*** {member.voice}
-        ***Guild-Permissions:*** {', '.join([perm.replace("_", " ").title() for perm, enabled in member.guild_permissions if enabled])}""")
+        ***Guild-Permissions:*** {', '.join([perm.replace("_", " ").title() for perm, enabled in member.guild_permissions if enabled])}""", inline=False)
         iombed.set_thumbnail(url=member.avatar.url)
         if image.banner and image.banner.url:
             iombed.set_image(url=image.banner.url)
