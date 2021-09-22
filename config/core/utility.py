62336c4241e37b54c2d9a472cadf0d4c6be53639
lvlahraam <lvlahraam@gmail.com>
2021-09-22 04:31:05 +0330

    Update utility.py

--- config/core/utility.py
+++ config/core/utility.py
@@ -137,27 +137,27 @@ class Utility(commands.Cog, description="Useful commands that are open to everyo
     @commands.command(name="spotify", help="Will show your or the given member's spotify activity if possible", usage="[member]")
     async def spotify(self, ctx, member:commands.MemberConverter = None):
         member = member or ctx.author
-        activity = {activity for activity in member.activities}
-        if isinstance(activity, discord.Spotify):
-            finspotifymbed = discord.Embed(
-                colour=self.bot.colour,
-                url=activity.track_url,
-                title=activity.title
-            )
-            finspotifymbed.add_field(name="Artists:", value=", ".join(artist for artist in activity.artists))
-            finspotifymbed.add_field(name="Album", value=activity.album)
-            finspotifymbed.add_field(name="Duration:", value=activity.duration)
-            finspotifymbed.add_field(name="Created-at:", value=activity.created_at)
-            finspotifymbed.add_field(name="Track-ID", value=activity.track_id)
-            finspotifymbed.set_image(url=activity.album_cover_url)
-            await ctx.send(embed=finspotifymbed) 
-        else:
-            badspotifymbed = discord.Embed(
-                colour=self.bot.colour,
-                title="You are not listenning to Spotofy"
-            )
-            badspotifymbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
-            await ctx.send(embed=badspotifymbed) 
+        for activity in member.activities:
+            if isinstance(activity, discord.Spotify):
+                finspotifymbed = discord.Embed(
+                    colour=self.bot.colour,
+                    url=activity.track_url,
+                    title=activity.title
+                )
+                finspotifymbed.add_field(name="Artists:", value=", ".join(artist for artist in activity.artists))
+                finspotifymbed.add_field(name="Album", value=activity.album)
+                finspotifymbed.add_field(name="Duration:", value=activity.duration)
+                finspotifymbed.add_field(name="Created-at:", value=activity.created_at)
+                finspotifymbed.add_field(name="Track-ID", value=activity.track_id)
+                finspotifymbed.set_image(url=activity.album_cover_url)
+                await ctx.send(embed=finspotifymbed) 
+            else:
+                badspotifymbed = discord.Embed(
+                    colour=self.bot.colour,
+                    title=F"{member.name} isn't are not listenning to Spotify"
+                )
+                badspotifymbed.set_footer(text=ctx.author, icon_url=ctx.author.avatar.url)
+                await ctx.send(embed=badspotifymbed) 
 
     # Source
     @commands.command(name="source", aliases=["src"], help="Will show the bots source", usage="[module]")
