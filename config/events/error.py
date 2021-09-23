1761df01dd8f99265fd8b102ef5808410ad309f0
lvlahraam <lvlahraam@gmail.com>
2021-09-23 16:25:01 +0330

    Update error.py

--- config/events/error.py
+++ config/events/error.py
@@ -10,7 +10,7 @@ class OnError(commands.Cog):
     async def on_command_error(self, ctx:commands.Context, error):
         if isinstance(error, commands.CommandInvokeError):
             error = error.original
-            await ctx.send()
+            await ctx.send(error)
         elif isinstance(error, commands.NotOwner):
             nombed = discord.Embed(
                 colour=self.bot.colour,
