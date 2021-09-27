ecf09ee36da26548d68606882999b2903292aa30
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-27 18:54:19 +0330

    Update fun.py

--- config/commands/fun.py
+++ config/commands/fun.py
@@ -10,6 +10,7 @@ class Fun(commands.Cog, description="You sad?. Use these to at least have a smil
         class NitroButton(discord.ui.Button):
             def __init__(self, view, **kwargs):
                 super().__init__(**kwargs)
+                self.bot = view.bot
             async def callback(self, interaction:discord.Interaction):
                 if self.label == "Claim":
                     anitrombed = discord.Embed(
@@ -26,8 +27,9 @@ class Fun(commands.Cog, description="You sad?. Use these to at least have a smil
                 else:
                     print("ELSE")
         class NitroView(discord.ui.View):
-            def __init__(self):
+            def __init__(self, bot):
                 super().__init__(timeout=15)
+                self.bot = bot
                 self.add_item(item=NitroButton(label="Claim", style=discord.ButtonStyle.green, view=self))
             async def on_timeout(self):
                 if self.children:
@@ -35,7 +37,7 @@ class Fun(commands.Cog, description="You sad?. Use these to at least have a smil
                         self.clear_items()
                         self.add_item(discord.ui.Button(emoji="💣", label="You took so long to answer...", style=discord.ButtonStyle.red, disabled=True))
                         await self.message.edit(view=self)
-        view = NitroView()
+        view = NitroView(bot=self.bot)
         bnitrombed = discord.Embed(
             colour=self.bot.colour,
             title="Click the button for claiming Nitro",
