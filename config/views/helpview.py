5a5f908f04753ec76efb47c008ca517977dbbc0f
lvlahraam <lvlahraam@gmail.com>
2021-09-27 16:36:56 +0330

    Update helpview.py

--- config/views/helpview.py
+++ config/views/helpview.py
@@ -24,7 +24,7 @@ class PaginatorView(discord.ui.View):
                 )
             embed.set_thumbnail(url=self.help.context.me.avatar.url)
             embed.set_author(name=self.help.context.author, icon_url=self.help.context.author.avatar.url)
-            embed.set_footer(text=F"<> is required | [] is optional | Page: {len(self.embeds)}/{len(cog)}")
+            embed.set_footer(text=F"<> is required | [] is optional | Page: {len(self.embeds)}/{}")
             self.embeds.append(embed)
 
     @discord.ui.button(emoji="⏯", style=discord.ButtonStyle.green)
