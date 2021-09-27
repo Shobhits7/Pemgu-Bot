29290d21dd7f0e567ed3707ab6e55fc94dd5667b
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-27 12:35:57 +0330

    Update helpview.py

--- config/views/helpview.py
+++ config/views/helpview.py
@@ -23,7 +23,7 @@ class PaginatorView(discord.ui.View):
             embed.description += "\n".join(gts(cmd) for cmd in cmds)
             self.embeds.append(embed)
         
-    @discord.ui.button(emoji="⏮", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(emoji="⏮", label="Previous", style=discord.ButtonStyle.blurple)
     async def previous(self, button:discord.ui.Button, interaction:discord.Interaction):
         if self.embed == 0:
             self.disabled = True
@@ -31,15 +31,15 @@ class PaginatorView(discord.ui.View):
             self.embed -= 1
             await interaction.response.edit_message(embed=self.embeds[self.embed])
     
-    @discord.ui.button(emoji="⏹", style=discord.ButtonStyle.red)
+    @discord.ui.button(emoji="⏹", label="Stop", style=discord.ButtonStyle.red)
     async def delete(self, button:discord.ui.Button, interaction:discord.Interaction):
         await interaction.message.delete()
 
-    @discord.ui.button(emoji="⏯", style=discord.ButtonStyle.green)
+    @discord.ui.button(emoji="⏯", label="Homepage", style=discord.ButtonStyle.green)
     async def homepage(self, button:discord.ui.Button, interaction:discord.Interaction):
         await interaction.response.edit_message(embed=self.homepage)
 
-    @discord.ui.button(emoji="⏭", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(emoji="⏭", label="Next", style=discord.ButtonStyle.blurple)
     async def next(self, button:discord.ui.Button, interaction:discord.Interaction):
         if self.embed == 7 or self.embed == 8:
             self.disabled = True
