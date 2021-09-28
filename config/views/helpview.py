f252781008d9e188e304dfc1fef6752313f88595
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-09-28 18:16:55 +0330

    Update helpview.py

--- config/views/helpview.py
+++ config/views/helpview.py
@@ -123,7 +123,7 @@ class SelectView(discord.ui.View):
                 option = discord.SelectOption(emoji=self.help.emojis.get(name) if self.help.emojis.get(name) else '❓', label=name, description=description, value=name)
                 options.append(option)
         self.add_item(item=SelectUI(placeholder="Where do you want to go...", options=options, min_values=1, max_values=1, view=self))
-        self.add_item(item=discord.ui.Button(emoji="🧇", label="Add Me", url=discord.utils.oauth_url(client_id=self.help.context.me.id, scopes=('bot', 'applications.commands'), permissions=discord.Permissions(administrator=True)), row=1))
+        self.add_item(item=discord.ui.Button(emoji="🧇", label="Add Me", url=discord.utils.oauth_url(client_id=self.help.context.me.id, scopes=('bot', 'applications.commands'), permissions=discord.Permissions(administrator=True)), row=0))
         self.add_item(item=discord.ui.Button(emoji="🍩", label="Support Server", url="https://discord.gg/bWnjkjyFRz", row=3))
 
     @discord.ui.button(emoji="💣", label="Delete", style=discord.ButtonStyle.red, row=4)
