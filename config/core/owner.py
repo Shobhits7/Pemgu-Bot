e1aaf41af9743b4a6c13570fb6d8549e4a64df98
lvlahraam <lvlahraam@gmail.com>
2021-09-28 23:45:42 +0330

    Update owner.py

--- config/core/owner.py
+++ config/core/owner.py
@@ -81,6 +81,9 @@ class Owner(commands.Cog, description="Only lvlahraam can use these commands"):
     @commands.command(name="repeat", help="Will repeat the given commands the amounts of time given", usage="<time> <command>")
     @commands.is_owner()
     async def repeat(self, ctx:commands.Context, time:int, command:str):
+        print(command)
+        for _ in command:
+            print(_)
         for _ in range(1, time+1):
             await self.bot.process_commands(command)
         await ctx.send(F"Successfully repeated `{command}` - `{time}` times")
