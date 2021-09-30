b9669c62101ed4259c8bea7ec959fa175c9b999c
lvlahraam <lvlahraam@gmail.com>
2021-09-30 19:39:21 +0330

    Update owner.py

--- config/core/owner.py
+++ config/core/owner.py
@@ -1,9 +1,55 @@
-import discord, os, sys
+import discord, os, sys, io, textwrap, traceback, contextlib
 from discord.ext import commands
 
 class Owner(commands.Cog, description="Only lvlahraam can use these commands"):
     def __init__(self, bot):
         self.bot = bot
+        self._last_result = None
+
+    @commands.command(name='eval', help="Evaluates a code", usage="<body>")
+    async def _eval(self, ctx, *, body: str):
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'channel': ctx.channel,
+            'author': ctx.author,
+            'guild': ctx.guild,
+            'message': ctx.message,
+            '_': self._last_result
+        }
+
+        env.update(globals())
+
+        body = self.cleanup_code(body)
+        stdout = io.StringIO()
+
+        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+        try:
+            exec(to_compile, env)
+        except Exception as e:
+            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+        func = env['func']
+        try:
+            with contextlib.redirect_stdout(stdout):
+                ret = await func()
+        except Exception as e:
+            value = stdout.getvalue()
+            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+        else:
+            value = stdout.getvalue()
+            try:
+                await ctx.message.add_reaction('\u2705')
+            except:
+                pass
+
+            if ret is None:
+                if value:
+                    await ctx.send(f'```py\n{value}\n```')
+            else:
+                self._last_result = ret
+                await ctx.send(f'```py\n{value}{ret}\n```')
 
     # Load
     @commands.command(name="load", help="Will load the given cog if it was not already loaded", usage="<cog>")
