e9f412aa5712cf7fb57839d8b37eec4ff0b152ef
lvlahraam <lvlahraam@gmail.com>
2021-09-30 16:49:09 +0330

    Update anime.py

--- config/core/anime.py
+++ config/core/anime.py
@@ -2,8 +2,9 @@ import discord
 from discord.ext import commands
 
 class Paginator(discord.ui.View):
-    def __init__(self, embeds):
+    def __init__(self, ctx, embeds):
         super().__init__(timeout=5)
+        self.ctx = ctx
         self.page = 1
         self.embeds = embeds
 
@@ -26,6 +27,26 @@ class Paginator(discord.ui.View):
             await interaction.response.edit_message(content="There are no more quotes", view=button.view)
         self.page += 1
         await interaction.response.edit_message(embed=self.embeds[self.page])
+
+    async def on_timeout(self):
+        try:
+            self.clear_items()
+            await self.message.edit(view=self)
+        except discord.NotFound:
+            return
+
+    async def interaction_check(self, interaction:discord.Interaction):
+        if interaction.user.id == self.ctx.author.id:
+            return True
+        icheckmbed = discord.Embed(
+            colour=self.ctx.bot.colour,
+            title="You can't use this",
+            description=F"<@{interaction.user.id}> - Only <@{self.ctx.author.id}> can use that\nCause they did the command\nIf you wanted to use the command, do what they did",
+            timestamp=self.ctx.message.created_at
+        )
+        icheckmbed.set_author(name=interaction.user, icon_url=interaction.user.display_avatar.url)
+        await interaction.response.send_message(embed=icheckmbed, ephemeral=True)
+        return False
 
 class Anime(commands.Cog, description="Some Weeb shit"):
     def __init__(self, bot):
