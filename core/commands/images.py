552b7357b9d6445b13a144cd99f051407cd6fe4b
lvlahraam <lvlahraam@gmail.com>
2021-10-04 14:25:17 +0330

    Update images.py

--- core/commands/images.py
+++ core/commands/images.py
@@ -15,7 +15,7 @@ class Images(commands.Cog, description="Free Photoshop, without needing to know 
         data = BytesIO(await asset.read())
         pfp = Image.open(data)
         pfp = pfp.resize((204, 204))
-        wanted.paste(pfp, (112, 200))
+        wanted.paste(pfp, (108, 200))
         wanted.save("wanted.png")
         wantedmbed = discord.Embed(
             colour=self.bot.colour,
