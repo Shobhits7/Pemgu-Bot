ed8c7ccec65d84dd492c0fb51dea6d6b5ba6fe2e
lvlahraam <lvlahraam@gmail.com>
2021-10-04 12:12:19 +0330

    .

--- core/commands/image.py
+++ core/commands/image.py
@@ -10,7 +10,7 @@ class Image(commands.Cog, description="Free Photoshop, without needing to know a
     @commands.command(name="wanted")
     async def wanted(self, ctx:commands.Context, user:discord.User=None):
         user = ctx.author if not user else user
-        wanted = Image.open("./images/wanted.jpg")
+        wanted = Image.open("../images/wanted.jpg")
         pfp = user.avatar.url
         buffer = io.BytesIO(await pfp.read())
         image = Image.open(buffer)
@@ -19,7 +19,7 @@ class Image(commands.Cog, description="Free Photoshop, without needing to know a
         wanted.save(buffer, "jpg")
         buffer.seek(0)
         final = io.BytesIO(await image.read())
-        await ctx.send(file=discord.File(fp=final, filename="whatever.jpg"))
+        await ctx.send(file=discord.File(fp=final, filename="wanted.jpg"))
 
 def setup(bot):
     bot.add_cog(Image(bot))
