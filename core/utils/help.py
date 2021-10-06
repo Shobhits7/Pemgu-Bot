8cf4f3ba9ac370a50c5df2fb20d3e02258f1fe6e
lvlahraam <lvlahraam@gmail.com>
2021-10-06 21:35:47 +0330

    Update help.py

--- core/utils/help.py
+++ core/utils/help.py
@@ -65,7 +65,7 @@ class CustomHelp(commands.HelpCommand):
             name = cog.qualified_name if cog else "No"
             description = cog.description if cog else "Commands without category"
             cmds = cog.walk_commands() if cog else commands
-            view.homepage.add_field(name=name, value=F"{description}\n{', '.join(command for command in cmds)}")
+            view.homepage.add_field(name=name, value=', '.join(command.name for command in cmds))
         view.homepage.add_field(name="Buttons:", value="🏡 = The Homepage of this help\n💣 = Will delete this help\n🧇 = The url for adding this bot\n🍩 = The url for the support server for this bot\n👨‍💻 = The url for the source code on github for this bot")
         view.homepage.add_field(name="Prefix:", value=self.context.prefix or "In DM you don't need to use prefix")
         view.homepage.add_field(name="Arguments:", value="[] means the argument is optional.\n<> means the argument is required.\n***DO NOT USE THESE WHEN DOING A COMMAND***")
