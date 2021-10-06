126c986f5fcf81da79998533bb2fd5b3472c6be3
lvlahraam <lvlahraam@gmail.com>
2021-10-07 01:40:27 +0330

    Update gameview.py

--- core/views/gameview.py
+++ core/views/gameview.py
@@ -44,19 +44,19 @@ class RPSButtons(discord.ui.Button):
             self.useroption = "Scissors"
         tierpsmbed = discord.Embed(
             colour=self.ctx.bot.colour,
-            description=F"We both chose **{self.botoption}**, It's a tie",
+            description=F"We both chose **{self.botoption}**, It's a tie :/ :|",
             timestamp=interaction.message.created_at
         )
         tierpsmbed.set_footer(text=interaction.user, icon_url=interaction.user.display_avatar.url)
         wonrpsmbed = discord.Embed(
             colour=self.ctx.bot.colour,
-            description=F"You chose **{self.useroption}**, But, I chose **{self.botoption}**, You won! / I lost",
+            description=F"You chose **{self.useroption}**, But, I chose **{self.botoption}**, You won :) / I lost :(",
             timestamp=interaction.message.created_at
         )
         wonrpsmbed.set_footer(text=interaction.user, icon_url=interaction.user.display_avatar.url)
         lostrpsmbed = discord.Embed(
             colour=self.ctx.bot.colour,
-            description=F"I chose **{self.botoption}**, But, You chose **{self.useroption}**, I won! / You lost!",
+            description=F"I chose **{self.botoption}**, But, You chose **{self.useroption}**, I won :) / You lost :(",
             timestamp=interaction.message.created_at
         )
         lostrpsmbed.set_footer(text=interaction.user, icon_url=interaction.user.display_avatar.url)
