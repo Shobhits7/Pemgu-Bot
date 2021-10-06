17fe42704e1dc662242d22577368ad6702177868
lvlahraam <lvlahraam@gmail.com>
2021-10-06 11:59:02 +0330

    Changed the way that mention works

--- core/events/message.py
+++ core/events/message.py
@@ -8,8 +8,8 @@ class OnMessage(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message:discord.Message):
         if message.author.bot: return
-        if F"<@!{self.bot.user.id}>" in message.content or F"<@{self.bot.user.id}>" in message.content:
-            prefix = await self.bot.postgres.fetch("SELECT prefix FROM prefixes WHERE guild_id=$1", message.guild.id)
+        if message.content in (F"<@{self.bot.user.id}>", F"<@!{self.bot.user.id}>"):
+            prefix = await self.bot.postgres.fetchval("SELECT prefix FROM prefixes WHERE guild_id=$1", message.guild.id)
             pfmbed = discord.Embed(
                 colour=self.bot.colour,
                 title=F"My Prefix here is:",
