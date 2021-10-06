67e6ba4b4d0ceaa6065d9916e69223f20b818231
lvlahraam <lvlahraam@gmail.com>
2021-10-06 12:10:46 +0330

    Added Slash Commands

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ class JakeTheDogBase(commands.AutoShardedBot):
         return page.Paginator(self, embeds)
 
 bot = JakeTheDogBase(
-    slash_command=True,
+    slash_commands=True,
     slash_command_guilds=[804380398296498256],
     command_prefix=get_prefix,
     strip_after_prefix=True,
