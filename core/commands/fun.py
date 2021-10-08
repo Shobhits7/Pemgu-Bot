ca6aa4e2f72d889389fe4914783aca1e6dfe5003
lvlahraam <lvlahraam@gmail.com>
2021-10-08 13:59:19 +0330

    Changed the position of some commands

--- core/commands/fun.py
+++ core/commands/fun.py
@@ -7,6 +7,24 @@ class Fun(commands.Cog, description="You sad?. Use these to at least have a smil
         self.bot = bot
         self.dagpi_headers = {"Authorization": os.getenv("DAGPI")}
 
+    # Say
+    @commands.command(name="say", help="Will say your message")
+    async def say(self, ctx:commands.Context, *, say:str):
+        await ctx.send(F"{say} | {ctx.author.mention}")
+
+    # Nitro
+    @commands.command(name="nitro", help="Will gift free Nitro")
+    async def nitro(self, ctx:commands.Context):
+        bnitrombed = discord.Embed(
+            colour=self.bot.colour,
+            title="A WILD NITRO GIFT APPEARS?!",
+            description="Expires in 48 hours\nClick the button for claiming Nitro:.",
+            timestamp=ctx.message.created_at
+        )
+        bnitrombed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
+        view = fv.NitroView(ctx)
+        view.message = await ctx.send(embed=bnitrombed, view=view)
+
     # Roast
     @commands.command(name="roast", aliases=["rst"], help="Will roast you or the given user")
     async def roast(self, ctx:commands.Context, user:discord.User=None):
@@ -70,23 +88,6 @@ class Fun(commands.Cog, description="You sad?. Use these to at least have a smil
         twmbed.set_image(url="attachment://tweet.png")
         twmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
         await ctx.send(file=discord.File(fp=response, filename="tweet.png"), embed=twmbed)
-    # Say
-    @commands.command(name="say", help="Will say your message")
-    async def say(self, ctx:commands.Context, *, say:str):
-        await ctx.send(F"{say} | {ctx.author.mention}")
-
-    # Nitro
-    @commands.command(name="nitro", help="Will gift free Nitro")
-    async def nitro(self, ctx:commands.Context):
-        bnitrombed = discord.Embed(
-            colour=self.bot.colour,
-            title="A WILD NITRO GIFT APPEARS?!",
-            description="Expires in 48 hours\nClick the button for claiming Nitro:.",
-            timestamp=ctx.message.created_at
-        )
-        bnitrombed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
-        view = fv.NitroView(ctx)
-        view.message = await ctx.send(embed=bnitrombed, view=view)
 
 def setup(bot):
     bot.add_cog(Fun(bot))
