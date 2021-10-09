daf35292f5eda7d19eced481cf2fc2e83df81ee1
lvlahraam <lvlahraam@gmail.com>
2021-10-09 12:15:37 +0330

    Created Todo Commands and Cog

---
+++ core/commands/todo.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+class Todo(commands.Cog, description="If you are so lazy to do stuff, use these"):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.group(name="todo", help="Will show your list of tasks, consider using subcommands", invoke_without_command=True)
+    async def todo(self, ctx:commands.Context):
+        todos = await self.bot.fetch("SELECT * FROM todos WHERE user_id=$1", ctx.author.id)
+        tasks = []
+        counter = 1
+        if not todos: return await ctx.send("You currently don't have any tasks")
+        for task in todos:
+            tasks.append(F"{counter} - {task}\n")
+            counter += 1
+        todombed = discord.Embed(
+            colour=self.bot.colour,
+            title="Your current tasks:",
+            description="".join(task for task in tasks),
+            timestamp=ctx.message.created_at
+        )
+        todombed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar)
+        await ctx.send(embed=todombed)
+
+def setup(bot):
+    bot.add_cog(Todo(bot))
