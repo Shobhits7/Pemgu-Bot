f68c24499e6623fb5ee66d812f082ff2c85429ff
lvlahraam <lvlahraam@gmail.com>
2021-10-12 03:22:03 +0330

    Update error.py

--- core/events/error.py
+++ core/events/error.py
@@ -36,23 +36,12 @@ class OnError(commands.Cog):
         else:
             print("".join(traceback.format_exception(type(error), error,  error.__traceback__)))
             errors = {
-                commands.NotOwner: "You are not the owner of this bot",
-                commands.CommandOnCooldown: F"Command **{ctx.invoked_with}** on Cooldown",
-                commands.DisabledCommand: F"**{ctx.invoked_with}** is disabled",
-                commands.UserNotFound: "Couldn't find the given user",
-                commands.MemberNotFound: "Couldn't find the given member",
-                commands.RoleNotFound: "Couldn't find the given role",
-                commands.EmojiNotFound: "Couldn't find the the given emoji",
-                commands.NSFWChannelRequired: F"**{ctx.invoked_with}** is only possible in NSFW channels",
-                commands.PrivateMessageOnly: F"**{ctx.invoked_with}** can only be used in DMs",
-                commands.NoPrivateMessage: F"Can't use **{ctx.invoked_with}** in DMs",
                 commands.CheckFailure: F"<:shut:744345896912945214> YOU <:shut:744345896912945214> ARE  <:shut:744345896912945214> IN <:shut:744345896912945214> BLACKLIST <:shut:744345896912945214> YOU <:shut:744345896912945214> S-KID/IDIOT\nSAY <:shut:744345896912945214> YOU <:shut:744345896912945214> ARE <:shut:744345896912945214> SORRY <:shut:744345896912945214> {ctx.author.mention}",
-                discord.Forbidden: "Your problem is one of the problems mentioned below\n\nYou are the owner of this Server:\nIf you are the owner, and you are trying to use a changing info command, you can't\n\nCouldn't send messages to the user:\nThe user is not accepting messages from the members in here or just have this bot blocked\n\nUser's Position:\nThe user's position is higher than me or you"
             }
             errormbed = discord.Embed(
                 colour=self.bot.colour,
-                title="❌ There was a problem ❓",
-                description=errors[error] if error in errors else error,
+                title="❌ There was a problem",
+                description=errors.get(error) if error in errors else error,
                 timestamp=ctx.message.created_at
             )
             errormbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
