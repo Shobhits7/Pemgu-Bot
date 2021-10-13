31acf93a1efec6bab5e8c1c7f60fc34d32fd7be4
lvlahraam <lvlahraam@gmail.com>
2021-10-13 13:32:28 +0330

    Added Confirm

---
+++ core/views/confirm.py
@@ -0,0 +1,33 @@
+import discord
+
+class Confirm(discord.ui.View):
+    def __init__(self, ctx):
+        super().__init__()
+        self.value = None
+
+    @discord.ui.button(label="Confirm", style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message("Confirming", ephemeral=True)
+        self.value = True
+
+    @discord.ui.button(label="Cancel", style=discord.ButtonStyle.red)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        await interaction.response.send_message("Cancelling", ephemeral=True)
+        self.value = False
+    
+    async def on_timeout(self):
+        await self.message.delete()
+    
+    async def interaction_check(self, interaction:discord.Interaction):
+        if interaction.user.id == self.ctx.message.author.id:
+            return True
+        else:
+            icheckmbed = discord.Embed(
+                colour=self.ctx.bot.colour,
+                title=F"You can't use this",
+                description=F"<@{interaction.user.id}> - Only <@{self.ctx.message.author.id}> can use this\nCause they did the command\nIf you want to use this, do what they did",
+                timestamp=interaction.message.created_at
+            )
+            icheckmbed.set_author(name=interaction.user, icon_url=interaction.user.display_avatar.url)
+            await interaction.response.send_message(embed=icheckmbed, ephemeral=True)
+            return False
