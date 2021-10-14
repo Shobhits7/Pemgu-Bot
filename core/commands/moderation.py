51f5eda107ba5f1251e643b6bd26d1d47dc56d4a
lvlahraam <lvlahraam@gmail.com>
2021-10-14 19:42:03 +0330

    Removed is_owner()

--- core/commands/moderation.py
+++ core/commands/moderation.py
@@ -114,7 +114,6 @@ class Moderation(commands.Cog, description="Was someone being bad?"):
 
     # Lock
     @commands.command(name="lock", aliases=["lc"], help="Will lock the given channel", hidden=True)
-    @commands.is_owner()
     @commands.guild_only()
     @commands.has_guild_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
@@ -136,7 +135,6 @@ class Moderation(commands.Cog, description="Was someone being bad?"):
 
     # Unlock
     @commands.command(name="unlock", aliases=["ulc"], help="Will unlock the given channel", hidden=True)
-    @commands.is_owner()
     @commands.guild_only()
     @commands.has_guild_permissions(manage_channels=True)
     @commands.bot_has_guild_permissions(manage_channels=True)
@@ -185,9 +183,9 @@ class Moderation(commands.Cog, description="Was someone being bad?"):
             )
             crmtmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
             await ctx.send(content=muterole.mention, embed=crmtmbed)
-            for channel in ctx.guild.channels:
-                if not channel.permissions_synced:
-                    await channel.set_permissions(muterole, add_reactions=False, connect=False, speak=False, stream=False, send_messages=False, send_messages_in_threads=False, send_tts_messages=False, create_instant_invite=False, create_public_threads=False, create_private_threads=False)
+        overwrites = {muterole: discord.PermissionOverwrite(add_reactions=False, connect=False, speak=False, stream=False, send_messages=False, send_messages_in_threads=False, send_tts_messages=False, create_instant_invite=False, create_public_threads=False, create_private_threads=False)}
+        for channel in ctx.guild.channels:
+            await channel.edit(overwrites=overwrites)
         if muterole in member.roles:
             mtmbed.title = F"Successfully Un-Muted"
             mtmbed.description = F"UnMuted: {member.mention}\nReason: {reason}"
