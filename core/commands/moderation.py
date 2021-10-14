dc301df1d2ebae7af39cb5fb3014f804d906ff2d
lvlahraam <lvlahraam@gmail.com>
2021-10-14 22:59:46 +0330

    Update moderation.py

--- core/commands/moderation.py
+++ core/commands/moderation.py
@@ -183,8 +183,8 @@ class Moderation(commands.Cog, description="Was someone being bad?"):
             )
             crmtmbed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar.url)
             await ctx.send(content=muterole.mention, embed=crmtmbed)
-        for channel in ctx.guild.channels:
-            await channel.set_permissions(muterole, add_reactions=False, connect=False, speak=False, stream=False, send_messages=False, send_messages_in_threads=False, send_tts_messages=False, create_instant_invite=False, create_public_threads=False, create_private_threads=False)
+            for channel in ctx.guild.channels:
+                await channel.set_permissions(muterole, add_reactions=False, connect=False, speak=False, stream=False, send_messages=False, send_messages_in_threads=False, send_tts_messages=False, create_instant_invite=False, create_public_threads=False, create_private_threads=False)
         if muterole in member.roles:
             mtmbed.title = F"Successfully Un-Muted"
             mtmbed.description = F"UnMuted: {member.mention}\nReason: {reason}Role: {muterole.mention}"
