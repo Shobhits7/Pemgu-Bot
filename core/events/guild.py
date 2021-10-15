239287b75360a4417d9b17c098b5689e567df244
lvlahraam <lvlahraam@gmail.com>
2021-10-15 08:48:53 +0330

    Update guild.py

--- core/events/guild.py
+++ core/events/guild.py
@@ -10,6 +10,12 @@ class OnGuild(commands.Cog):
         prefix = await self.bot.postgres.fetchval("SELECT prefix FROM prefixes WHERE guild_id=$1", guild.id)
         if prefix:
             await self.bot.postgres.execute("DELETE FROM prefixes WHERE guild_id=$1", guild.id)
+        welcome = await self.bot.postgres.fetchval("SELECT msg FROM welcome WHERE guild_id=$1", guild.id)
+        if welcome:
+            await self.bot.postgres.execute("DELETE FROM welcome WHERE guild_id=$1", guild.id)
+        goodbye = await self.bot.postgres.fetchval("SELECT msg FROM goodbye WHERE guild_id=$1", guild.id)
+        if goodbye:
+            await self.bot.postgres.execute("DELETE FROM goodbye WHERE guild_id=$1", guild.id)
 
 def setup(bot):
     bot.add_cog(OnGuild(bot))
