b5402d13ca470db145448706b9eb705d9511f0e6
lvlahraam <lvlahraam@gmail.com>
2021-10-15 23:20:13 +0330

    Update command.py

--- core/events/command.py
+++ core/events/command.py
@@ -7,14 +7,15 @@ class OnCommand(commands.Cog):
     
     @commands.Cog.listener()
     async def on_command(self, ctx:commands.Context):
-        rimbed = discord.Embed(
-            color=self.bot.color,
-            title="Please re-invite me again for slash commands",
-            description="Since discord is forcing every bot to use slash commands `/` please re invite me again, with the command `.m invite`",
-            timestamp=ctx.message.created_at
-        )
-        rimbed.set_footer(text="From the Pemgu-Bot Developers", icon_url=self.bot.user.avatar.url)
-        await ctx.send(embed=rimbed)
+        if random.randint(0, 10) == 9:
+            rimbed = discord.Embed(
+                color=self.bot.color,
+                title="Please re-invite me again for slash commands",
+                description="Since discord is forcing every bot to use slash commands `/` please re invite me again, with the command `.m invite`",
+                timestamp=ctx.message.created_at
+            )
+            rimbed.set_footer(text="From the Pemgu-Bot Developers", icon_url=self.bot.user.avatar.url)
+            await ctx.send(embed=rimbed)
 
 def setup(bot):
     bot.add_cog(OnCommand(bot))
