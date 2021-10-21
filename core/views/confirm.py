5e3b4f40ddec78d4247bc3ccef484f585682fbca
lvlahraam <lvlahraam@gmail.com>
2021-10-21 11:14:04 +0330

    Update confirm.py

--- core/views/confirm.py
+++ core/views/confirm.py
@@ -2,7 +2,7 @@ import discord
 
 class Confirm(discord.ui.View):
     def __init__(self, ctx):
-        super().__init__(timeout=10)
+        super().__init__(timeout=3)
         self.ctx = ctx
         self.value = None
 
