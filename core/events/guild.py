bcfebed90b9725865703242f031f7ef3d3520809
lvlahraam <lvlahraam@gmail.com>
2021-10-23 20:58:08 +0330

    Added on_guild_join Event

--- core/events/guild.py
+++ core/events/guild.py
@@ -1,10 +1,21 @@
-import discord
+import discord, random
 from discord.ext import commands
 
 class OnGuild(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild:discord.Guild):
+        channel = random.choice(guild.text_channels)
+        ogjmbed = discord.Embed(
+            title="Thanks for inviting me!",
+            description=F"\nHey there! Thanks for inviting me!\nIf you need any help, just type **{self.bot.prefix}help**",
+            timestamp=discord.utils.utcnow()
+        )
+        ogjmbed.set_footer(text=F"From {self.bot.user.name} Developers", icon_url=self.bot.user.avatar.url)
+        await channel.send(embed=ogjmbed)
+
     @commands.Cog.listener()
     async def on_guild_remove(self, guild:discord.Guild):
         prefix = await self.bot.postgres.fetchval("SELECT prefix FROM prefixes WHERE guild_id=$1", guild.id)
