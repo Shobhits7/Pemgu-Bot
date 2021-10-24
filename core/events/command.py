1b4d4f5337779f5b2899e0c77826ab9042365588
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-10-25 00:31:37 +0330

    Update command.py

--- core/events/command.py
+++ core/events/command.py
@@ -7,7 +7,8 @@ class OnCommand(commands.Cog):
     
     @commands.Cog.listener()
     async def on_command(self, ctx:commands.Context):
-        if random.randint(0, 69) == 21:
+        number = random.randint(0, 100)
+        if number == 55:
             if "use_slash_commands" not in ctx.me.guild_permissions:
                 scmdmbed = discord.Embed(
                     color=self.bot.color,
@@ -17,6 +18,7 @@ class OnCommand(commands.Cog):
                 )
                 scmdmbed.set_footer(text="From the Pemgu-Bot Developers", icon_url=self.bot.user.avatar.url)
                 await ctx.send(embed=scmdmbed)
+        if number == 70:
             votembed = discord.Embed(
                 color=self.bot.color,
                 url="https://top.gg/bot/844226171972616205",
