c7efdccd33bdd441e99056bfbe5c47a111633b15
lvlahraam <lvlahraam@gmail.com>
2021-10-26 21:10:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ class PemguBase(commands.AutoShardedBot):
         self.load_extension("jishaku")
         os.environ["JISHAKU_NO_UNDERSCORE"] = "True"
         os.environ["JISHAKU_NO_DM_TRACEBACK"] = "True"
-        self.bot.afks = {}
+        self.afks = {}
 
     async def close(self):
         if not self.session.closed:
