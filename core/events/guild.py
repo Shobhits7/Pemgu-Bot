c45adc1906890ceae32428be036af99c4b45089b
lvlahraam <79662326+lvlahraam@users.noreply.github.com>
2021-10-28 17:27:19 +0330

    Update guild.py

--- core/events/guild.py
+++ core/events/guild.py
@@ -7,7 +7,7 @@ class OnGuild(commands.Cog):
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild:discord.Guild):
-        self.bot prefixes[guild.id] = bot.default_prefix
+        self.bot.prefixes[guild.id] = bot.default_prefix
         channel = random.choice(guild.text_channels)
         ogjmbed = discord.Embed(
             title="Thanks for inviting me!",
